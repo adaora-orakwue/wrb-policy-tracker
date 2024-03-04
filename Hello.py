c76f06ff47af061de826c72fc418caee57dc15b7
Adaora Orakwue <60138871+adaora-orakwue@users.noreply.github.com>
2024-03-04 18:18:13 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,57 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
+import pandas as pd
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
 
-    st.write("# Welcome to Streamlit! This is my first app!👋")
+# Create a function for the policy search dashboard
+def policy_search():
+   
 
-    st.sidebar.success("Select a demo above.")
+    # Load data
+    @st.cache_data
+    def load_data():
+        data = pd.read_csv('fullpolicylist.csv')  
+        return data
 
+    data = load_data()
+    
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
+        <style>
+            body {
+                background-color: #D67B82; /* Pink */
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
     )
 
+    st.title("Policy Search 🔍")
+
+    st.sidebar.header("Filter options")
+
+    # Select State
+    state = st.sidebar.selectbox('Select State', options=data['State'].unique())
+
+    # Select Year using a dropdown menu
+    year = st.sidebar.selectbox('Select Year', options=data['Year'].unique())
+    
+     # Create a link to the landing page
+    landing_page_link = "[Back to Landing Page](https://clever-state-2zd5.figment.so/)"
+    st.sidebar.markdown(landing_page_link, unsafe_allow_html=True)
+    
+
+    filtered_data = data[(data['State'] == state) & (data['Year'] == year)]
+
+    if not filtered_data.empty:
+        st.dataframe(filtered_data.style.format({"Year": "{:}"}))
+    else:
+        st.write("No policies found for the selected criteria.")
+
+# Main function to decide which page to show based on user input
+def main():
+  
+  policy_search()
 
 if __name__ == "__main__":
-    run()
+    main()
